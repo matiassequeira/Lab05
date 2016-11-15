cf164fb949ac4afc4d72fd40bd947edefbd2a8b5
matiassequeira <matiassequeirq@gmail.com>
2016-11-15 10:59:20 -0300

    Commit in English

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/lab05/AltaTareaActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/lab05/AltaTareaActivity.java
@@ -1,16 +1,26 @@
 package dam.isi.frsf.utn.edu.ar.lab05;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import dam.isi.frsf.utn.edu.ar.lab05.dao.ProyectoDAO;
 import dam.isi.frsf.utn.edu.ar.lab05.dao.ProyectoDBMetadata;
 import dam.isi.frsf.utn.edu.ar.lab05.modelo.Prioridad;
@@ -25,6 +35,7 @@ public class AltaTareaActivity extends AppCompatActivity implements View.OnClick
     Spinner responsable;
     Button btnGuardar;
     Button btnCancelar;
+    Button btnAgregarUsuario;
     ProyectoDAO proyectoDAO;
     boolean actualizacion;
     int id_tarea;
@@ -39,6 +50,7 @@ public class AltaTareaActivity extends AppCompatActivity implements View.OnClick
         responsable=(Spinner) findViewById(R.id.spinner);
         btnGuardar=(Button) findViewById(R.id.btnGuardar);
         btnCancelar=(Button) findViewById(R.id.btnCancelar);
+        btnAgregarUsuario = (Button) findViewById(R.id.btnAgregarUsuario);
 
         Cursor cursorSpinner = new ProyectoDAO(this).listaUsuarios();
         SpinnerAdapter adapter=new SimpleCursorAdapter(this,android.R.layout.simple_spinner_dropdown_item, cursorSpinner,
@@ -48,6 +60,7 @@ public class AltaTareaActivity extends AppCompatActivity implements View.OnClick
 
         btnGuardar.setOnClickListener(this);
         btnCancelar.setOnClickListener(this);
+        btnAgregarUsuario.setOnClickListener(this);
 
 
         Intent intent= this.getIntent();
@@ -59,11 +72,93 @@ public class AltaTareaActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnCancelar:
+                finish();
                 break;
             case R.id.btnGuardar:
                 guardarTarea();
                 break;
+            case R.id.btnAgregarUsuario:
+                abrirBusqueda();
+                break;
         }
+    }
+
+    private void abrirBusqueda() {
+        // Creating alert Dialog with one Button
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+        //AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle("Buscar usuario");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("Introduzca usuario");
+
+        final EditText input = new EditText(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+
+        // Setting Positive "Yes" Button
+        alertDialog.setPositiveButton("Buscar",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int which) {
+                       buscarContacto(input.getText().toString());
+                    }
+                });
+        // Setting Negative "NO" Button
+        alertDialog.setNegativeButton("Cancelar",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog
+                        dialog.cancel();
+                    }
+                });
+
+        // closed
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    private void buscarContacto(String nombreBuscado) {
+        JSONArray arr = new JSONArray();
+        final StringBuilder resultado = new StringBuilder();
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+
+        // consulta ejemplo buscando por nombre visualizado en los contactos agregados
+        Cursor c =this.getContentResolver().query(uri, null, ContactsContract.Contacts.DISPLAY_NAME+
+                " LIKE '"+nombreBuscado+"%'", null, sortOrder);
+
+        int count = c.getColumnCount();
+        int fila = 0;
+        String[] columnas= new String[count];
+        try {
+            while(c.moveToNext()) {
+                JSONObject unContacto = new JSONObject();
+                for(int i = 0; (i < count );  i++) {
+                    if(fila== 0)
+                        columnas[i]=c.getColumnName(i);
+                    unContacto.put(columnas[i],c.getString(i));
+                }
+                Log.d("TEST-ARR",unContacto.toString());
+                arr.put(fila,unContacto);
+                fila++;
+                Log.d("TEST-ARR","fila : "+fila);
+
+                // elegir columnas de ejemplo
+                resultado.append(unContacto.get("name_raw_contact_id")
+                        +" - "+unContacto.get("display_name")+
+                        System.getProperty("line.separator"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("TEST-ARR",arr.toString());
     }
 
     private void guardarTarea() {
